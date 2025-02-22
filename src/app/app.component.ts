7d7d1394d35982c7861194891ece5e1610aee13d
jafestro <jafestro@gmail.com>
2025-02-22 19:44:48 +0200

    Add game functionality and styles for dot game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'fill-with-dots';
+  dots = Array.from(Array(160).keys())
+  colors = [
+    'red',
+    'green',
+    'blue',
+    'pink',
+    'purple',
+    'orange',
+    'yellow',
+    'cyan',
+    'grey',
+    'magenta',
+  ];
+  randomColorIndex = 0;
+  counter = 25;
+  myinterval: any;
+  isGameStarted = false;
+
+  changeStyle(dot: any) {
+    this.randomColorIndex = Math.floor(Math.random() * this.colors.length);
+    dot.classList.add(this.colors[this.randomColorIndex]);
+    this.checkGameStatus();
+  }
+
+  startTimer() {
+    this.isGameStarted = true;
+    this.myinterval = setInterval(() => {
+      if (this.counter > 0) {
+        this.counter--;
+      }
+    }, 1000);
+  }
+
+  checkGameStatus() {
+    if (!this.isGameStarted){
+      this.startTimer();
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+      clearInterval(this.myinterval);
+  }
 }
